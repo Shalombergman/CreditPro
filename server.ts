96e73b13225c7c103261eb6c7f8bc0439883f371
Shalom Bergman <119497630+Shalombergman@users.noreply.github.com>
2024-11-18 16:48:36 +0200

    שינויים בהתקנות של אקספרס

---
+++ server.ts
@@ -0,0 +1,7 @@
+import express from 'express';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+import cors from 'cors';
+
+const app = express();
+const httpServer = createServer(app); 

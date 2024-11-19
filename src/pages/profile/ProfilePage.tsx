105420c505abb1aeb8c39d1c90aa89f8d36ee214
Shalom Bergman <119497630+Shalombergman@users.noreply.github.com>
2024-11-19 10:00:40 +0200

    תיקוני באגים

--- src/pages/profile/ProfilePage.tsx
+++ src/pages/profile/ProfilePage.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Card } from '@/components/ui/card';
+// import React from 'react';
+// import { Card } from '@/components/ui/card';
 
 export default function ProfilePage() {
   return (
